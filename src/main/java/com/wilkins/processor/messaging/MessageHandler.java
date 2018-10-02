58ed4654330e3245cabb36fcafec2e8e74dbda62
Robin Wilkins <robin@comcarde.com>
2018-10-02 16:24:37 +0100

    simplify the solution, but using spring Processor class

--- src/main/java/com/wilkins/processor/messaging/MessageHandler.java
+++ src/main/java/com/wilkins/processor/messaging/MessageHandler.java
@@ -5,20 +5,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 @Component
-@EnableBinding(MessagingChannels.class)
+@EnableBinding(Processor.class)
 @Slf4j
 @RequiredArgsConstructor
 public class MessageHandler {
 
     private final Tracer tracer;
 
-    @StreamListener(MessagingChannels.DEMO_REQUEST)
-    @SendTo(MessagingChannels.DEMO_RESPONSE)
+    @StreamListener(Processor.INPUT)
+    @SendTo(Processor.OUTPUT)
     public Message<String> handleMessage(Message<String> message) {
         log.info("Message Handled: {}, headers: {}", message.getPayload(), message.getHeaders());
         String traceId = tracer.currentSpan().context().traceIdString();
